a1d96acc70bdd15552e5e74e133bcca2d02a297c
Nastya Sorokova <ngrigis35@gmail.com>
2024-11-11 12:27:11 +0300

    first commit

---
+++ 2/index.js
@@ -0,0 +1,15 @@
+const alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+let randomWord = "";
+for (let i = 0; i < 4; i++) {
+        const randomIndex = Math.floor(Math.random() * alphabet.length);
+    
+    randomWord += alphabet[randomIndex];
+}
+
+н
+console.log(`Случайное слово из четырёх букв: ${randomWord}`);
+makeEight.innerText = num1 % 10
+
+
+
